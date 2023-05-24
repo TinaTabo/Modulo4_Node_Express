b93c33b9ffab06f954756a29bbcb9e31452a5bea
Cristina Taboada <cris.taboada98@gmail.com>
2023-05-24 13:55:08 +0200

    reto 1 completo + documentacion jsdoc

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Modulo4_Node_Express
+## Modulo 4: Node y Express
 Este repositorio contiene todos los retos del Módulo 4 del Bootcamp de Desarrollo Web y Móvil de Codenotch
---
+++ src/RetosTema1/reto1.js
@@ -0,0 +1,9 @@
+/**
+ * Programa que muestra por consola mensajes pasado un intervalo de tiempo.
+ */
+console.log('Mensaje 1');
+
+setTimeout(() => {
+    console.log('Mensaje 2');
+    console.log('Mensaje 3');
+}, 3000);
