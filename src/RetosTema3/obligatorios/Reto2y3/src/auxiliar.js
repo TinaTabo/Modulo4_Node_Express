8d7e77727bc81edb503fa2cd7b8c06bf9a7f5044
Cristina Taboada <cris.taboada98@gmail.com>
2023-05-30 13:49:47 +0200

    Reto 3 completo

--- src/RetosTema3/obligatorios/Reto2y3/src/auxiliar.js
+++
@@ -1,8 +0,0 @@
-//-- Base de datos hardcoded de libros
-let book1 = new Book('Viuda de Hierro','Tapa blanda','Xiran Jay Zhao',18.05,'https://imagessl4.casadellibro.com/a/l/t7/04/9788427224704.jpg',982305,1234);
-let book2 = new Book('Ready Player One','Tapa blanda','Ernest Cline',15.00,'https://imagessl9.casadellibro.com/a/l/t7/79/9788466649179.jpg',743219,2345);
-let book3 = new Book('Trono de Cristal','Tapa blanda','Sarah J. Maas',14.50,'https://imagessl6.casadellibro.com/a/l/t7/86/9788418359286.jpg',561890,3456);
-let book4 = new Book('Harry Potter y la Piedra Filosofal','Tapa dura','J.K.Rowling',14.75,'https://imagessl2.casadellibro.com/a/l/t7/62/9788498382662.jpg',256734,4567);
-let book5 = new Book('Cuando los Árboles Cantan','Tapa blanda','Laura Gallego',12.95,'https://imagessl0.casadellibro.com/a/l/t7/30/9788467550030.jpg',629108,5678);
-
-let books = [book1,book2,book3,book4,book5];
--- src/RetosTema3/obligatorios/Reto2y3/src/controller/books.controller.js
+++ src/RetosTema3/obligatorios/Reto2y3/src/controller/books.controller.js
@@ -2,23 +2,43 @@
 const Book = require('../models/book');
 
 //-- Base de datos hardcoded de libros
-let book = new Book('Viuda de Hierro','Tapa blanda','Xiran Jay Zhao',18.05,'https://imagessl4.casadellibro.com/a/l/t7/04/9788427224704.jpg',982305,1234);
-//let book = null;
+let book1 = new Book('Viuda de Hierro','Tapa blanda','Xiran Jay Zhao',18.05,'https://imagessl4.casadellibro.com/a/l/t7/04/9788427224704.jpg',1,1234);
+let book2 = new Book('Ready Player One','Tapa blanda','Ernest Cline',15.00,'https://imagessl9.casadellibro.com/a/l/t7/79/9788466649179.jpg',2,2345);
+let book3 = new Book('Trono de Cristal','Tapa blanda','Sarah J. Maas',14.50,'https://imagessl6.casadellibro.com/a/l/t7/86/9788418359286.jpg',3,3456);
+let book4 = new Book('Harry Potter y la Piedra Filosofal','Tapa dura','J.K.Rowling',14.75,'https://imagessl2.casadellibro.com/a/l/t7/62/9788498382662.jpg',4,4567);
+let book5 = new Book('Cuando los Árboles Cantan','Tapa blanda','Laura Gallego',12.95,'https://imagessl0.casadellibro.com/a/l/t7/30/9788467550030.jpg',5,5678);
+
+let books = [book1,book2,book3,book4,book5];
 
 //-- Funciones del servidor
 
-//-- Obtiene el libro
+//-- Obtiene el libro con la id pasada por parámetro. Si no se le pasa ningún id
+//-- devuelve el array completo de libros.
 const getBook = (req,res)=>{
     let answer;
-    if (book != null) {
-        answer = book;
+    if (books != null) {
+        if (req.query.id != undefined) {
+            let i = 0;
+
+            while (req.query.id != books[i].id_book && i < books.length) {
+                i++;
+            }
+
+            if (req.query.id == books[i].id_book) {
+                answer = books[i];
+            }else{
+                answer = "No existe ningún libro con la id solicitada"
+            }
+        }else{
+            answer = books;
+        }
     }else{
         answer = "No existe ningún libro";
     }
     res.send(answer);
 }
 
-//-- Crear un nuevo objeto libro
+//-- Crear un nuevo objeto libro + RETO3: lo añade al array de libros.
 //-- RETO 2: para las pruebas se envía en el body de la solicitud POST lo siguiente:
 /*
 {
@@ -32,52 +52,59 @@ const getBook = (req,res)=>{
 }
 */
 const postBook = (req,res)=>{
-    if (book == null) {
-        book = new Book(req.body.title,req.body.type,req.body.author,req.body.price,req.body.photo,req.body.id_book,req.body.id_user);
-    }
-    let answer = book;
+    let book = new Book(req.body.title,req.body.type,req.body.author,req.body.price,req.body.photo,req.body.id_book,req.body.id_user);
+    books.push(book)
+    let answer = "Libro añadido correctamente";
     res.send(answer);
 }
 
-//-- Modifica los datos del libro enviados por el body de la solicitud PUT.
+//-- Modifica los datos del libro cuyo id coincida con los datos enviados por el body de la solicitud PUT.
 const putBook = (req,res)=>{
     let answer;
-    if (book != null) {
+    let i = 0;
+    while (req.body.id_book != books[i].id_book && i < books.length) {
+        i++;
+    }
+
+    if (req.body.id_book == books[i].id_book) {
         if (req.body.title != undefined) {
-            book.title = req.body.title;
+            books[i].title = req.body.title;
         }
         if (req.body.type != undefined) {
-            book.type = req.body.type;
+            books[i].type = req.body.type;
         }
         if (req.body.author != undefined) {
-            book.author = req.body.author;
+            books[i].author = req.body.author;
         }
         if (req.body.price != undefined) {
-            book.price = req.body.price;
+            books[i].price = req.body.price;
         }
         if (req.body.photo != undefined) {
-            book.photo = req.body.photo;
+            books[i].photo = req.body.photo;
         }
         if (req.body.id_book != undefined) {
-            book.id_book = req.body.id_book;
+            books[i].id_book = req.body.id_book;
         }
         if (req.body.id_user != undefined) {
-            book.id_user = req.body.id_user;
+            books[i].id_user = req.body.id_user;
         }
-    }else{
-        answer = "No existe ningún libro"
+        answer = "Libro modificado correctamente"
     }
+
     res.send(answer);
 }
 
 //-- Elimina un libro
 const delBook = (req,res)=>{
     let answer;
-    if (book != null) {
-        book = null;
+    let i = 0;
+    while (req.body.id_book != books[i].id_book && i < books.length) {
+        i++;
+    }
+
+    if (req.body.id_book == books[i].id_book) {
+        books.splice(i,1);
         answer = "Libro eliminado correctamente";
-    }else{
-        answer = "No existe ningún libro para eliminar";
     }
     res.send(answer);
 }
