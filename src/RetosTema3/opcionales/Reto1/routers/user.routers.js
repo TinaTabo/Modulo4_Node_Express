34e328d6169a5fa04db5e7aaef2d559472c76e4a
Cristina Taboada <cris.taboada98@gmail.com>
2023-06-03 10:49:16 +0200

    copia apiBooks dia3

--- src/RetosTema3/opcionales/Reto1/routers/user.routers.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require("express");
-const router = Router();
-const usersCtrl = require("../controller/user.controller");
-
-
-router.get("/", usersCtrl.getUser);
-router.post("/", usersCtrl.postUser);
-
-module.exports = router;
